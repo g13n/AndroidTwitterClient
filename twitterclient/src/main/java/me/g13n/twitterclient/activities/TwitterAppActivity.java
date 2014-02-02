1916eb7a74f8b13038c3a0a46abbb42f5506ee83
Gopal Venkatesan <gv@g13n.me>
2014-02-02 13:11:29 -0800

    First cut changes with creating a fragment for main view.

--- twitterclient/src/main/java/me/g13n/twitterclient/activities/TwitterAppActivity.java
+++ twitterclient/src/main/java/me/g13n/twitterclient/activities/TwitterAppActivity.java
@@ -1,39 +1,29 @@
 package me.g13n.twitterclient.activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.AbsListView;
-import android.widget.ListView;
-
-import com.activeandroid.ActiveAndroid;
-
-import org.json.JSONArray;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import me.g13n.twitterclient.R;
-import me.g13n.twitterclient.adapters.TweetsAdapter;
-import me.g13n.twitterclient.helpers.BaseJsonHttpResponseHandler;
-import me.g13n.twitterclient.helpers.TwitterClient;
-import me.g13n.twitterclient.helpers.TwitterClientApp;
-import me.g13n.twitterclient.models.Tweet;
+import me.g13n.twitterclient.fragments.TimelineFragment;
 
-public class TwitterAppActivity extends Activity {
+public class TwitterAppActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_twitter_app);
 
-        bindUI();
-        preloadTweets();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new TimelineFragment())
+                    .commit();
+        }
     }
 
     @Override
@@ -86,7 +76,7 @@ public class TwitterAppActivity extends Activity {
 
 
     public void onRefresh(MenuItem item) {
-        refreshTimeline();
+//        refreshTimeline();
     }
 
 
@@ -97,9 +87,9 @@ public class TwitterAppActivity extends Activity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == COMPOSE_ACTIVITY && resultCode == RESULT_OK) {
-            refreshTimeline();
-        }
+//        if (requestCode == COMPOSE_ACTIVITY && resultCode == RESULT_OK) {
+//            refreshTimeline();
+//        }
 
         super.onActivityResult(requestCode, resultCode, data);
     }
@@ -114,86 +104,11 @@ public class TwitterAppActivity extends Activity {
     }
 
 
-    private void preloadTweets() {
-        List<Tweet> tweets = TwitterClientApp.getTweets();
-        Log.v("preloadTweets", "Returning " + tweets.size() + " tweets from the database");
-        for (Tweet tweet : tweets) {
-            tweetsAdapter.add(tweet);
-        }
-        tweetsAdapter.notifyDataSetChanged();
-    }
-
-    private void refreshTimeline() {
-        TwitterClient twitterClient = (TwitterClient) TwitterClientApp.getClient();
-        twitterClient.getHomeTimeline(lastId,
-                new BaseJsonHttpResponseHandler(getBaseContext(), getString(R.string.error_service),
-                        getString(R.string.error_network)) {
-
-            @Override
-            public void onSuccess(JSONArray jsonResults) {
-                ArrayList<Tweet> tweets = Tweet.fromJSON(jsonResults);
-                int numItems = tweets.size();
-                if (numItems > 0) {
-                    /*
-                     * If this is the first time refreshTimeline is being called, the list might
-                     * have old tweets from the previous session (restored from the database.)
-                     * Just wipe and start over again.
-                     */
-                    if (!hasRefreshed) {
-                        tweetsAdapter.clear();
-                        hasRefreshed = true;
-                        Log.v("refreshTimeline", "clearing the list ...");
-                    }
-
-                    ActiveAndroid.beginTransaction();
-                    lastId = tweets.get(numItems - 1).getTweetId();
-                    try {
-                        for (Tweet tweet : tweets) {
-                            tweetsAdapter.add(tweet);
-                            tweet.save();
-                        }
-                        tweetsAdapter.notifyDataSetChanged();
-                        ActiveAndroid.setTransactionSuccessful();
-                    } finally {
-                        ActiveAndroid.endTransaction();
-                    }
-                }
-            }
-
-        });
-    }
-
-
-    private void bindUI() {
-        tweetsAdapter = new TweetsAdapter(this, new ArrayList<Tweet>());
-        ListView lvTweets = (ListView) findViewById(R.id.lvTweets);
-        lvTweets.setAdapter(tweetsAdapter);
-        lvTweets.setOnScrollListener(new AbsListView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
-
-            }
-
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
-                                 int totalItemCount) {
-                if (firstVisibleItem + visibleItemCount >= totalItemCount) {
-                    refreshTimeline();
-                }
-            }
-        });
-    }
-
-
     private final int COMPOSE_ACTIVITY = 2;
     private final int LOGIN_ACTIVITY = 1;
 
 
-    private TweetsAdapter tweetsAdapter;
-
-    private long lastId = 0;
     private Menu menu;
     private boolean isLoggedIn = true;
-    private boolean hasRefreshed = false;
 
 }
---
+++ twitterclient/src/main/java/me/g13n/twitterclient/fragments/TimelineFragment.java
@@ -0,0 +1,121 @@
+package me.g13n.twitterclient.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ListView;
+
+import com.activeandroid.ActiveAndroid;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.g13n.twitterclient.R;
+import me.g13n.twitterclient.adapters.TweetsAdapter;
+import me.g13n.twitterclient.helpers.BaseJsonHttpResponseHandler;
+import me.g13n.twitterclient.helpers.TwitterClient;
+import me.g13n.twitterclient.helpers.TwitterClientApp;
+import me.g13n.twitterclient.models.Tweet;
+
+public class TimelineFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_timeline, container, false);
+
+        refreshTimeline();
+        bindUI();
+
+        return rootView;
+    }
+
+
+    private void bindUI() {
+        tweetsAdapter = new TweetsAdapter(getActivity().getBaseContext(), new ArrayList<Tweet>());
+        ListView lvTweets = (ListView) rootView.findViewById(R.id.lvTweets);
+        lvTweets.setAdapter(tweetsAdapter);
+        lvTweets.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+                                 int totalItemCount) {
+                if (firstVisibleItem + visibleItemCount >= totalItemCount) {
+                    refreshTimeline();
+                }
+            }
+        });
+    }
+
+
+    private void refreshTimeline() {
+        TwitterClient twitterClient = TwitterClientApp.getClient();
+        twitterClient.getHomeTimeline(lastId,
+                new BaseJsonHttpResponseHandler(getActivity().getBaseContext(),
+                        getString(R.string.error_service), getString(R.string.error_network)) {
+
+                    @Override
+                    public void onSuccess(JSONArray jsonResults) {
+                        ArrayList<Tweet> tweets = Tweet.fromJSON(jsonResults);
+                        int numItems = tweets.size();
+                        if (numItems > 0) {
+                    /*
+                     * If this is the first time refreshTimeline is being called, the list might
+                     * have old tweets from the previous session (restored from the database.)
+                     * Just wipe and start over again.
+                     */
+                            if (!hasRefreshed) {
+                                tweetsAdapter.clear();
+                                hasRefreshed = true;
+                                Log.v("refreshTimeline", "clearing the list ...");
+                            }
+
+                            ActiveAndroid.beginTransaction();
+                            lastId = tweets.get(numItems - 1).getTweetId();
+                            try {
+                                for (Tweet tweet : tweets) {
+                                    tweetsAdapter.add(tweet);
+                                    tweet.save();
+                                }
+                                tweetsAdapter.notifyDataSetChanged();
+                                ActiveAndroid.setTransactionSuccessful();
+                            } finally {
+                                ActiveAndroid.endTransaction();
+                            }
+                        }
+                    }
+
+                });
+    }
+
+
+    private void preloadTweets() {
+        List<Tweet> tweets = TwitterClientApp.getTweets();
+        Log.v("preloadTweets", "Returning " + tweets.size() + " tweets from the database");
+        for (Tweet tweet : tweets) {
+            tweetsAdapter.add(tweet);
+        }
+        tweetsAdapter.notifyDataSetChanged();
+    }
+
+
+    // State variables
+    private long lastId = 0;
+    private boolean hasRefreshed = false;
+
+
+    private View rootView;
+
+
+    private TweetsAdapter tweetsAdapter;
+
+}
